6750c35cbd1926a6ea6bc121b8e4bbb4444af277
RichardPickman <drezzerock@gmail.com>
2022-07-22 00:38:54 +0200

    feat: add general index

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import { getServerConfig } from './config';
+import router from './Routes';
+import errorHandler from './middleware/ErrorHandle/index'
+import './model/db';
+
+
+const { port } = getServerConfig();
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use('/api', router);
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
