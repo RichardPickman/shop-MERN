5bd874950e357fe9ea8ea2ddd3dc50649c147b2d
RichardPickman <drezzerock@gmail.com>
2022-07-24 01:01:25 +0200

    feat: add cart route

--- server/Routes/index.ts
+++ server/Routes/index.ts
@@ -2,11 +2,13 @@ import Router from 'express';
 import characterRouter from './characterRouter';
 import itemRouter from './itemRouter';
 import userRouter from './userRouter';
+import cartRouter from './cartRouter';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/items', itemRouter);
 router.use('/characters', characterRouter);
+router.use('/cart', cartRouter);
 
 export default router;
