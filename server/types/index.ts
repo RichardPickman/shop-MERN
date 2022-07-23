3c64e48184673cd1ad321464a71aaefd8e38f597
RichardPickman <drezzerock@gmail.com>
2022-07-24 00:59:48 +0200

    feat: move all types to own file

--- server/types/index.ts
+++ server/types/index.ts
@@ -1,7 +1,36 @@
+import { Document } from 'mongoose';
 
-
-interface User {
+export interface User extends Document {
     name: string;
     main: string;
     password: string; 
+}
+
+export interface Product extends Document {
+    name: string,
+    thumbnail: string,
+    gallery: [],
+    characters: number[],
+    season: number[],
+    category: string,
+    count: number,
+    year: number,
+    color: string,
+    size: string,
+    favorite: false,
+    price: number,
+    discount: number,
+    shipping: number,
+    rating: number,
+}
+
+export interface CartProduct extends Document {
+    amount: number,
+    product: Product,
+    subTotal: number,
+}
+
+export interface Cart extends Document {
+    userId: string,
+    items: CartProduct[]
 }
